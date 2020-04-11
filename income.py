325c40ce2e2ed0f0b1c6f97e68c4ee9f4000d64a
Mohit Gaur <56885276+Mohit-Gaur@users.noreply.github.com>
2020-04-11 13:59:52 +0530

    Update income.py

--- income.py
+++ income.py
@@ -10,9 +10,13 @@ from sklearn.ensemble import RandomForestClassifier
 income_data = pd.read_csv('income.csv', header = 0, delimiter = ', ')
 #print(income_data.iloc[0])
 labels = income_data[['income']]
-data = income_data[['age', 'capital-gain', 'capital-loss', 'hours-per-week']]
-train_data, test_data, train_labels, test_labels = train_test_split(data, labels, random_state = 1)
 
+income_data["sex-int"] = income_data["sex"].apply(lambda row: 0 if row == "Male" else 1)
+income_data['country-int'] = income_data['native-country'].apply(lambda row: 0 if row == 'United States' else 1) #slightly improves accuracy
+
+data = income_data[['age', 'capital-gain', 'capital-loss', 'hours-per-week', 'sex-int', 'country-int']]
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, random_state = 1)
+#print(income_data["native-country"].value_counts())
 forest = RandomForestClassifier(random_state = 1)
 forest.fit(train_data, train_labels)
 print(forest.score(test_data, test_labels))
